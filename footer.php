eb8cfd96f48b4fedff172c8461f4bbabdac55445
Laravel215 <daniel.green.bigcommerce@gmail.com>
2021-05-04 10:03:52 +0530

    finished footer

---
+++ footer.php
@@ -0,0 +1,49 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <?php require_once "head.php" ?>
+</head>
+
+<body>
+    <footer class="container-fluid" id="footer">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-4" id="location">
+                <span>Location</span><br>
+                <div class="container-fluid" id="address">
+                    <span>No.34</span><br>
+                    <span>Wettuma kade junction</span><br>
+                    <sapn>Kalutara</sapn>
+                </div>
+
+                <hr class="d-block d-sm-none" id="line">
+            </div>
+
+            <div class="col-12 col-sm-12 col-md-4" id="socialTags">
+                <span>Follow us on</span>
+                <div class="ontainer-fluid" id="social">
+                    <div class="row" id="socialRow">
+                        <div class="col-3"><a href="#"><i class="bi bi-facebook"></i></a></div>
+                        <div class="col-3"><a href="#"><i class="bi bi-twitter"></i></a></div>
+                        <div class="col-3"><a href="#"><i class="bi bi-linkedin"></i></a></div>
+                        <div class="col-3"><a href="#"><i class="bi bi-github"></i></a></div>
+                    </div>
+                </div>
+                <hr class="d-block d-sm-none" id="line">
+            </div>
+
+            <div class="col-12 col-sm-12 col-md-4" id="helpCentre">
+                <span>Help Centre</span><br>
+                <div class="container-fluid" id="info">
+                    <span>aravindakolitha1998@gmail.com</span><br>
+                    <span>0713331978</span>
+                </div>
+                <hr class="d-block d-sm-none" id="line">
+            </div>
+        </div>
+    </footer>
+
+    <?php require_once "script.php" ?>
+</body>
+
+</html>
