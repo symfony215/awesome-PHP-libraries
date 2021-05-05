b599c5e5fec7e9b1ea6a0c77a1d1d3bf1d44fc62
Laravel215 <daniel.green.bigcommerce@gmail.com>
2021-05-05 23:21:27 +0530

    responsible navbar

--- nav.php
+++ nav.php
@@ -1,3 +1,22 @@
+<?php
+$count = 1;
+if ($count = 1) {
+    session_start();
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = "Aravinda";
+}
+$loginStatus = false;
+if (($_SESSION['loggedin']) && ($_SESSION['loggedin']) == true) {
+    $loginStatus = true;
+    // echo ("logged in");
+} else {
+    $loginStatus = false;
+    //echo ("not logged in please try again");
+}
+
+?>
+
+
 <DOCTYPE html>
     <html lang="en">
 
@@ -7,19 +26,19 @@
     </head>
 
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light" id="navigation">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav ml-auto" id="navbarItems">
                         <a class="nav-link" aria-current="page" href="#">Home</a>
-                        <a class="nav-link" href="#">Features</a>
-                        <a class="nav-link" href="#">Pricing</a>
-                        <a class="nav-link" href="#"><i class="bi bi-cart"></i></a>
-                        <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></a>
+                        <a class="nav-link" href="#">Catogaries</a>
+                        <a class="nav-link" href="#">About us</a>
+                        <a class="nav-link" href="#"><i class="bi bi-cart"> </i></a>
+                        <a class="nav-link" href="#"><i class="bi bi-person-circle"> hi <?= htmlentities($_SESSION['username']) ?></i></a>
 
                     </div>
                 </div>
@@ -27,5 +46,6 @@
         </nav>
     </body>
     <?PHP require_once "script.php" ?>
+    <?php require_once "footer.php" ?>
 
     </html>
