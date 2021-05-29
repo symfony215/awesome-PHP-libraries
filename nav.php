365cb567fedda3c82c934ed7b3340534c36d88e6
Laravel215 <daniel.green.bigcommerce@gmail.com>
2021-05-29 13:00:28 +0530

    fix whitespace and navbar

--- nav.php
+++ nav.php
@@ -17,42 +17,37 @@ if (($_SESSION['loggedin']) && ($_SESSION['loggedin']) == true) {
 ?>
 
 
-<DOCTYPE html>
-    <html lang="en">
 
-    <head>
-        <?php require_once "head.php" ?>
-        <link rel="stylesheet" href="./styles/navbar.css" />
-    </head>
+<!-- <head>
+    <link rel="stylesheet" href="./styles/navbar.css" />
+</head>
 
-    <body>
-        <nav class="navbar navbar-expand-md navbar-light" id="navigation">
-            <div class="container-fluid">
-                <a class="navbar-brand" id="brand" href="home.php">Thejana's Bookstore</a>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav ml-auto" id="navbarItems">
-                        <a class="nav-link" aria-current="page" href="#">Home</a>
-                        <a class="nav-link" href="#">Catogaries</a>
-                        <a class="nav-link" href="#">About us</a>
-                        <a class="nav-link" href="#"><i class="bi bi-cart"> </i></a>
-                        <a class="nav-link" href="#"><i class="bi bi-person-circle"> hi <?= htmlentities($_SESSION['username']) ?> </i></a>
+<body> -->
+<nav class="navbar navbar-expand-md navbar-light" id="navigation">
+    <div class="container-fluid">
+        <a class="navbar-brand" id="brand" href="home.php">Thejana's Bookstore</a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto" id="navbarItems">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Catogaries</a>
+                <a class="nav-link" href="#">About us</a>
+                <a class="nav-link" href="#"><i class="bi bi-cart"> </i></a>
+                <a class="nav-link" href="#"><i class="bi bi-person-circle"> hi <?= htmlentities($_SESSION['username']) ?> </i></a>
 
-                    </div>
-                </div>
-                <div class="dropdown d-md-none">
-                    <button onclick="myFunction()" class="dropbtn bi bi-list"></button>
-                    <div id="myDropdown" class="dropdown-content">
-                        <a class="nav-link" id="linknames" href="#"><i class="bi bi-person-circle"> hi <?= htmlentities($_SESSION['username']) ?> </i></a>
-                        <a class="nav-link" id="linknames" aria-current="page" href="home.php">Home</a>
-                        <a class="nav-link" id="linknames" href="#">Catogaries</a>
-                        <a class="nav-link" id="linknames" href="#">About us</a>
-                        <a class="nav-link" id="linknames" href="#"><i class="bi bi-cart"> </i></a>
-                    </div>
-                </div>
             </div>
-        </nav>
-    </body>
-    <?PHP require_once "script.php" ?>
-    <script src="js/nav.js"></script>
+        </div>
+        <div class="dropdown d-md-none">
+            <button onclick="myFunction()" class="dropbtn bi bi-list"></button>
+            <div id="myDropdown" class="dropdown-content">
+                <a class="nav-link" id="linknames" href="#"><i class="bi bi-person-circle"> hi <?= htmlentities($_SESSION['username']) ?> </i></a>
+                <a class="nav-link" id="linknames" aria-current="page" href="home.php">Home</a>
+                <a class="nav-link" id="linknames" href="#">Catogaries</a>
+                <a class="nav-link" id="linknames" href="#">About us</a>
+                <a class="nav-link" id="linknames" href="#"><i class="bi bi-cart"> </i></a>
+            </div>
+        </div>
+    </div>
+</nav>
 
-    </html>
+
+<script src="js/nav.js"></script>
