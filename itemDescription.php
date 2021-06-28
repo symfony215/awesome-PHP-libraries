d36d8c988e1a90a44f27d0186be815681b078322
Laravel215 <daniel.green.bigcommerce@gmail.com>
2021-06-28 11:08:18 +0530

    item des change

--- itemDescription.php
+++ itemDescription.php
@@ -28,6 +28,7 @@ while ($rows = $result->fetch_assoc()) {
     <link rel="stylesheet" href="./styles/footer.css">
     <link rel="stylesheet" href="./styles/navbar.css" />
     <link rel="stylesheet" href="./styles/description.css" />
+    <link rel="stylesheet" href="./styles/home_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     <?php require_once "head.php" ?>
 </head>
@@ -72,13 +73,91 @@ while ($rows = $result->fetch_assoc()) {
 
         </div>
         <div class="col-sm-2 col-md-3"></div>
+    <!--header2-->
+
+    <div class="container">
+        <h4>Top selling by <?= htmlentities($author) ?></h4>
+    </div>
+
+    <div class="container-fluid">
+        <hr>
+    </div>
+
+
+    <!--other books-->
+    <?php
+    require_once("./home/homeDb.php");
+    require_once("./home/item-component.php");
+
+    //Create instance of createdb class
+    // $database=new CreateDb("Productdb", "productb");
+
+    ?>
+
+    <div class="container">
+        <div class="row text-center py-5">
+            <?php
+            $sql = "SELECT * FROM productb WHERE product_author='$author' LIMIT 2,4";
+            $result = mysqli_query($con, $sql);
+            // $result = $database->getData();
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    component($row['product_name'], $row['product_price'], $row['second_price'], $row['product_image'], $row['id']);
+                }
+            }
+            ?>
+        </div>
+    </div>
+    <br>
+    <br>
+
+    <!--header1-->
+    <div class="container-fluid">
+        <div class="header" id="header1">
+            <h5>Top selling</h5>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <hr>
+    </div>
+
+
+    <!--top selling item-->
+    <?php
+    require_once("./home/homeDb.php");
+    require_once("./home/item-component.php");
+
+    //Create instance of createdb class
+    // $database=new CreateDb("Productdb", "productb");
+
+    ?>
+
+
+
+    <div class="container">
+        <div class="row text-center py-5">
+            <?php
+            $sql = "SELECT * FROM productb LIMIT 3,8";
+            $result = mysqli_query($con, $sql);
+            // $result = $database->getData();
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    component($row['product_name'], $row['product_price'], $row['second_price'], $row['product_image'], $row['id']);
+                }
+            }
+
+            ?>
+        </div>
+    </div>
+
 
     </div>
 
     <?php require_once "script.php" ?>
-    <footer id="footer1">
+    <!--<footer id="footer1">-->
         <?php require_once "footer.php" ?>
-    </footer>
+    <!--</footer>-->
+    
 
 </body>
 
