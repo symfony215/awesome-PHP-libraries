14d5089dffb1dd9bf4478bb64b92ace422602009
Laravel215 <daniel.green.bigcommerce@gmail.com>
2021-08-11 01:19:04 +0530

    Admin Login DB update

--- login_admin.php
+++ login_admin.php
@@ -21,7 +21,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: admin_login.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+		$sql = "SELECT * FROM admins WHERE user_name='$uname' AND password='$pass'";
 
 		$result = mysqli_query($conn, $sql);
 
